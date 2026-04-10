8dffb94f64932f8e96cfc7f484b7d54c5f97347b
serenafares <serena.fares@epfl.ch>
2026-04-10 13:57:51 +0200

    shado direction try

--- bproc_generator/render/generate_try.py
+++ bproc_generator/render/generate_try.py
@@ -948,7 +948,8 @@ def solar_to_blender_rotation(azimuth_deg: float, zenith_deg: float,
     """
     elevation_rad = math.radians(90 - zenith_deg)
     # Negate + apply north offset to go from meteorological to Blender azimuth
-    blender_az_rad = math.radians(-(azimuth_deg + north_offset_deg))
+    # blender_az_rad = math.radians(-(azimuth_deg + north_offset_deg))
+    blender_az_rad = math.radians(180 - azimuth_deg - north_offset_deg)
     return (elevation_rad, 0.0, blender_az_rad)
 
 
