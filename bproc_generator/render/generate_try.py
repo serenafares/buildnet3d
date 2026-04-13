9b3f607005b1e8661d0531478d1a56a2114af6e6
serenafares <serena.fares@epfl.ch>
2026-04-13 14:31:32 +0200

    Update generate_try.py

--- bproc_generator/render/generate_try.py
+++ bproc_generator/render/generate_try.py
@@ -946,12 +946,10 @@ def solar_to_blender_rotation(azimuth_deg: float, zenith_deg: float,
 
     Returns (elevation_rad, 0, blender_azimuth_rad)
     """
-    elevation_rad = math.radians(90 - zenith_deg)
+    # elevation_rad = math.radians(90 - zenith_deg)
+    elevation_rad = math.radians(zenith_deg - 90)
     # Negate + apply north offset to go from meteorological to Blender azimuth
-    blender_az_rad = math.radians(-(azimuth_deg + north_offset_deg))
-    # blender_az_rad = math.radians(azimuth_deg - 180 + north_offset_deg)
-    # blender_az_rad = math.radians(180 - azimuth_deg - north_offset_deg)
-
+    blender_az_rad = math.radians(180 - azimuth_deg - north_offset_deg)
 
     return (elevation_rad, 0.0, blender_az_rad)
 
