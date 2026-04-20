87ba671c3f0e9da01774837f2ebfc9569f5ac546
serenafares <serena.fares@epfl.ch>
2026-04-20 23:13:42 +0200

    Update render_day.bat

--- render_day.bat
+++ render_day.bat
@@ -10,7 +10,7 @@ for %%H in (04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21) do (
     for %%M in (00 30) do (
         set OUT=!BASE_OUT!\%%H_%%M
         echo Running %%H:%%M UTC ...
-        blenderproc run bproc_generator/render/generate_try.py ^
+        blenderproc run bproc_generator/render/generate_clean.py ^
             --latitude %LAT% ^
             --longitude %LON% ^
             --date_time "%DATE% %%H:%%M:00" ^
