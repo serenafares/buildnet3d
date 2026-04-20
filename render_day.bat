db8e334a32b778234203eae145248f9200061470
serenafares <serena.fares@epfl.ch>
2026-04-20 23:16:47 +0200

    Update render_day.bat

--- render_day.bat
+++ render_day.bat
@@ -6,7 +6,7 @@ set LON=6.6323
 set DATE=2026-03-21
 set BASE_OUT=C:\Users\sefares\Desktop\renders
 
-for %%H in (04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21) do (
+for %%H in (05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21) do (
     for %%M in (00 30) do (
         set OUT=!BASE_OUT!\%%H_%%M
         echo Running %%H:%%M UTC ...
