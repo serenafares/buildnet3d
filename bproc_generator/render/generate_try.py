4b2f26513f44d97619a4d4a47d1e067af34310d0
serenafares <serena.fares@epfl.ch>
2026-04-30 17:56:57 +0200

    0.6 ksun

--- bproc_generator/render/generate_try.py
+++ bproc_generator/render/generate_try.py
@@ -19,7 +19,7 @@ sys.path.extend([str(Path(__file__).resolve().parents[2])])
 from buildnet3d.utils.utils import build_translation
 
 # Physical calibration scalars
-K_SUN: float = 0.0075
+K_SUN: float = 0.006
 NISHITA_FILL_FACTOR: float = 11.0
 SHADOW_FILL_BOOST: float = 1.0
 CIVIL_TWILIGHT_ZENITH: float = 96.0
