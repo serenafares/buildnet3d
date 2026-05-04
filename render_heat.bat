aad16080d40a55a64610f65336af523f91bd9e9d
serenafares <serena.fares@epfl.ch>
2026-05-04 20:04:10 +0200

    Update render_heat.bat

--- render_heat.bat
+++ render_heat.bat
@@ -1,6 +1,9 @@
+
+
 python C:\Users\sefares\buildnet3d\bproc_generator\render\heat_flux.py ^
 --latitude 46.5 ^
 --longitude 6.6 ^
 --altitude 400 ^
 --date-time "2024-06-21 10:00:00" ^
---output-path Z:\heatflux
+--output-path Z:\heatflux ^
+--camera-poses "Z:\renders\10h00\meta_data.json"
