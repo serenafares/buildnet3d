25f9acc9693e126b64d6cfc855400d2f88f88b0b
serenafares <serena.fares@epfl.ch>
2026-05-27 19:31:41 +0200

    Create render_video.bat

---
+++ render_video.bat
@@ -0,0 +1,4 @@
+python C:\Users\sefares\buildnet3d\bproc_generator\render\heat_flux2.py ^
+  --latitude 46.5 --longitude 6.6 --altitude 400 ^
+  --date 2024-06-21 --output-path Z:\renders ^
+  --interval-minutes 10 --video-mode True --video-pose-index 11
